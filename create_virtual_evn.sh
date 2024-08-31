006e0be9b56afaf764df8fb2ad4a5702b95d8763
Sai Sandeep D U M <sandeepsai664@gmail.com>
2024-08-31 20:40:18 +0530

    modified the .sh file

--- create_virtual_evn.sh
+++ create_virtual_evn.sh
@@ -4,21 +4,21 @@
 CURRENT_DIR=$(pwd)
 
 # Extract the folder name from the current directory path
-FOLDER_NAME=$(basename "$CURRENT_DIR")
+FOLDER_NAME=$(basename "${CURRENT_DIR}_venv")
 
 # Create a virtual environment with the name of the folder
-python3 -m venv "${FOLDER_NAME}_venv"
+python3 -m venv "${FOLDER_NAME}"
 
 # Activate the virtual environment
-source "$FOLDER_NAME/bin/activate"
+source "${FOLDER_NAME}/bin/activate"
 
 # Upgrade pip
-pip3 install --upgrade pip
+pip install --upgrade pip
 
 # Install required libraries
-pip3 install requests numpy pandas ipykernel
+pip install requests numpy pandas ipykernel
 
 # Deactivate the virtual environment (optional)
-# deactivate
+deactivate
 
 echo "Virtual environment '$FOLDER_NAME' created and libraries installed."
