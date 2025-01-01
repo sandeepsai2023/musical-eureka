158a0324dedad00c0f7a9d0e61906302c9d74975
Sai Sandeep D U M <sandeepsai664@gmail.com>
2025-01-01 14:57:02 +0530

    Leetcode

---
+++ leetcode/main.py
@@ -0,0 +1,76 @@
+from typing import List
+
+# # class Solution:
+# #     def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+# #         nums1[m:]=nums2
+# #         print(nums1)
+# #         """
+# #         Do not return anything, modify nums1 in-place instead.
+# #         """
+        
+
+# # temp = Solution()
+# # temp.merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)
+# # # nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+
+# class Solution:
+#     def maxScoreSightseeingPair(self, values: List[int]) -> int:
+#         max_val = 0
+#         max_i = 0
+#         i=0
+#         j=i+1
+#         # for i in range(0,len(values)):
+#         while i<len(values):
+#             # for j in range(i+1,len(values)):
+#             max_i = values[i]+i
+#             # temp = (values[i] + values[j]) - (j - i)
+#             # values_2.append(temp)
+#             if max_val<temp:
+#                 max_val=temp
+#             else:
+#                 pass
+#             j = i+1
+#         return max_val
+    
+
+# print(Solution().maxScoreSightseeingPair(values = [8,1,5,2,6]))
+
+
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        # max_val = 0
+        # max_i = 0
+        # i=0
+        # j=i+1
+        # for i in range(0,len(values)):
+        # # while i<len(values):
+        #     for j in range(i+1,len(values)):
+        #         temp = (values[i] + values[j]) - (j - i)
+        #         # values_2.append(temp)
+        #         if max_val<temp:
+        #             max_val=temp
+        #         else:
+        #             pass
+        #         j = i+1
+        # return max_val
+
+        max_i = 0
+        max_val = 0
+        for j in range(1,len(values)):
+            i=j-1
+            temp_i = values[i]+i
+            if max_i<temp_i:
+                max_i = temp_i
+            
+            temp_value = max_i+values[j]-j
+            # if j==1:
+            #     max_val = temp_value
+            # else:
+            #     temp_value = max_i+values[j]-j
+            
+            if max_val<temp_value:
+                max_val = temp_value
+                # max_val = max_i+temp_value
+
+            # print(i,max_i,temp_value,max_val)
+        return max_val
